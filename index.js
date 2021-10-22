5fa7748148a23ecc7206f8a338ff13a5cbd03bb6
becca <rebecca.kaci@gmail.com>
2021-10-22 10:40:10 +0200

    exercice 3 terminé

--- index.js
+++ index.js
@@ -8,12 +8,18 @@ console.log(fruits);
 // EXERCICE 2 - UNSHIFT SANS UNSHIFT
 
 let fruitss = ['Apple', 'Orange', 'Banana']
+
 fruitss = ['Raspberry', ... fruitss]
 console.log(fruitss);
 
 
 // EXERCICE 3 - ARRAY CONCATENATION
 
+const arr1 = ['A', 'B', 'C']
+const arr2 = ['X', 'Y', 'Z']
+
+const arr3 = [...arr1,...arr2]
+console.log(arr3)
 
 
 // EXERCICE 4 - OBJECTS
